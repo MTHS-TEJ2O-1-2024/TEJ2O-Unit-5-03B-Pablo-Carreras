b59590c8b5ee275ce6f7fc11b5baa5e9483204c5
Pablo-Carreras <pablo.carreras-canton@mths.ca>
2024-09-27 15:23:09 +0000

    made code

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -6,5 +6,13 @@ This module is a Micro:bit MicroPython program
 
 from microbit import *
 
+display.clear()
+display.show(Image.HAPPY)
 
-display.scroll("Hello, World!")
+while True:
+    if button_a.is_pressed():
+        pin16.write_digital(1)
+        display.show(Image.YES)
+    if button_b.is_pressed():
+        pin16.write_digital(0)
+        display.show(Image.NO)
