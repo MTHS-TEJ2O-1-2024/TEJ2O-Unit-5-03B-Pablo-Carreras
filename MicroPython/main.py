7b442808cf0e91fa358b3c871a7567a4b386d51c
Pablo-Carreras <pablo.carreras-canton@mths.ca>
2024-09-27 15:23:33 +0000

    added space

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -13,6 +13,7 @@ while True:
     if button_a.is_pressed():
         pin16.write_digital(1)
         display.show(Image.YES)
+
     if button_b.is_pressed():
         pin16.write_digital(0)
         display.show(Image.NO)
